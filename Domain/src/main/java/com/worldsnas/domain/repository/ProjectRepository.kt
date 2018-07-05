21ae1d1b7758ac7b7e885ef294a5d8746f45af45
Seyed <worldsnas@gmail.com>
2018-07-05 09:47:26 +0430

    project repository created

---
+++ Domain/src/main/java/com/worldsnas/domain/repository/ProjectRepository.kt
@@ -0,0 +1,16 @@
+package com.worldsnas.domain.repository
+
+import com.worldsnas.domain.model.Project
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+interface ProjectRepository {
+
+    fun getProjects(): Observable<List<Project>>
+
+    fun bookmarkProject(projectId: String): Completable
+
+    fun unbookmarkProject(projectId: String): Completable
+
+    fun getBookmarkedProjects(): Observable<List<Project>>
+}
