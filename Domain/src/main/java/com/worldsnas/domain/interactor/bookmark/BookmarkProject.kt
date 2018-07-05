de331db8e1bc9337f9cbb39a918bed3bad9a6757
Seyed <worldsnas@gmail.com>
2018-07-05 12:27:58 +0430

    bookmark project usecase created

---
+++ Domain/src/main/java/com/worldsnas/domain/interactor/bookmark/BookmarkProject.kt
@@ -0,0 +1,26 @@
+package com.worldsnas.domain.interactor.bookmark
+
+import com.worldsnas.domain.executor.PostExecutionThread
+import com.worldsnas.domain.interactor.CompletableUseCase
+import com.worldsnas.domain.repository.ProjectRepository
+import io.reactivex.Completable
+import javax.inject.Inject
+
+class BookmarkProject @Inject constructor(
+        private val projectRepository: ProjectRepository,
+        postExecutionThread: PostExecutionThread) :
+        CompletableUseCase<BookmarkProject.Params>(postExecutionThread) {
+
+    override fun buildUseCaseCompletable(params: Params?): Completable {
+        if (params == null) throw IllegalArgumentException("Params can not be null!")
+        return projectRepository.bookmarkProject(params.projectId)
+    }
+
+    data class Params constructor(val projectId: String) {
+        companion object {
+            fun forProject(projectId: String): Params {
+                return Params(projectId)
+            }
+        }
+    }
+}
