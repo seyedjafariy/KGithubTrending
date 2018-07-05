bcd0234c47d1cb2f3c4191d50f5f3352d3402339
Seyed <worldsnas@gmail.com>
2018-07-05 15:59:29 +0430

    projects cache data store mpl created

---
+++ Data/src/main/java/com/worldsnas/data/store/ProjectCacheDataStore.kt
@@ -0,0 +1,37 @@
+package com.worldsnas.data.store
+
+import com.worldsnas.data.model.ProjectEntity
+import com.worldsnas.data.repository.ProjectsCache
+import com.worldsnas.data.repository.ProjectsDataStore
+import io.reactivex.Completable
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class ProjectCacheDataStore @Inject constructor(
+        private val projectsCache: ProjectsCache) :
+        ProjectsDataStore {
+    override fun getProjects(): Observable<List<ProjectEntity>> {
+        return projectsCache.getProjects()
+    }
+
+    override fun clearProjects(): Completable {
+        return projectsCache.clearProjects()
+    }
+
+    override fun saveProjects(projects: List<ProjectEntity>): Completable {
+        return projectsCache.saveProjects(projects)
+                .andThen(projectsCache.setLastCacheTime(System.currentTimeMillis()))
+    }
+
+    override fun getBookmarkProjects(): Observable<List<ProjectEntity>> {
+        return projectsCache.getBookmarkProjects()
+    }
+
+    override fun setProjectAsBookmark(projectId: String): Completable {
+        return projectsCache.setProjectAsBootkmark(projectId)
+    }
+
+    override fun setProjectAsNotBookmark(projectId: String): Completable {
+        return projectsCache.setProjectAsNotBootkmark(projectId)
+    }
+}
