04115b793761a2dcfd5fa5c7b952e4bc2ce04b89
worldsnas <worldsnas@gmail.com>
2018-07-13 11:54:47 +0430

    test data factory implemented

---
+++ presentation/src/test/java/com/worldsnas/presentation/factory/DataFactory.kt
@@ -0,0 +1,22 @@
+package com.worldsnas.presentation.factory
+
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
+
+object DataFactory {
+    fun randomUuid(): String {
+        return UUID.randomUUID().toString()
+    }
+
+    fun randomBoolean(): Boolean {
+        return Math.random() < 0.5
+    }
+
+    fun randomInt(): Int {
+        return ThreadLocalRandom.current().nextInt(0, 1000 + 1)
+    }
+
+    fun randomLong(): Long {
+        return randomInt().toLong()
+    }
+}
