4b57e0384a5246bc15eb780fbe7311d1ab9b9638
Seyed <worldsnas@gmail.com>
2018-08-02 16:46:54 +0430

    execution thread implemented

---
+++ mobile-ui/src/main/java/com/worldsnas/mobile_ui/UiThread.kt
@@ -0,0 +1,12 @@
+package com.worldsnas.mobile_ui
+
+import com.worldsnas.domain.executor.PostExecutionThread
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import javax.inject.Inject
+
+class UiThread @Inject constructor() : PostExecutionThread{
+
+    override val scheduler: Scheduler
+        get() = AndroidSchedulers.mainThread()
+}
