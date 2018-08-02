0379a8f844345e75db5d29b3aca5f87898ffccd5
Seyed <worldsnas@gmail.com>
2018-08-02 16:49:14 +0430

    app class created

---
+++ mobile-ui/src/main/java/com/worldsnas/mobile_ui/GithubTrendingApplication.kt
@@ -0,0 +1,17 @@
+package com.worldsnas.mobile_ui
+
+import android.app.Application
+import timber.log.Timber
+
+class GithubTrendingApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupTimber()
+    }
+
+    private fun setupTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
+}
